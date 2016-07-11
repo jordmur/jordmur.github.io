166c1bf6dc12959d8da08c5adf7f20734996e09a
jordmur <jordanmurphy11@gmail.com>
2016-07-11 10:48:02 -0500

    (feat) Removes JS files

--- app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $portfolioCards = $('.single-portfolio');
-  console.log($portfolioCards);
-  $portfolioCards.on('click', function (e) {
-    console.log(this);
-  })
-})
